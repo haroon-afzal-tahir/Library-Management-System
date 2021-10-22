fd5851a95716c9fcc3f39ed6903ba9a974c9b7a3
Haroon Tahir <noono14252@outlook.com>
2021-10-22 23:44:15 +0500

    Admin SQL Implemented

--- Library Management System/Database/Admin.sql
+++ Library Management System/Database/Admin.sql
@@ -5,4 +5,10 @@ CREATE TABLE Admin (
 	/*
 		Admin_ID, Name, Picture_Path, Age, CNIC, Phone Number
 	*/
+	[Admin ID] INT NOT NULL PRIMARY KEY IDENTITY(1, 1),
+	[Admin Name] VARCHAR(20) NOT NULL,
+	[Picture Directory] VARCHAR(50),
+	Age INT NOT NULL,
+	CNIC VARCHAR(15) UNIQUE,
+	[Phone Number] VARCHAR(12)
 )
