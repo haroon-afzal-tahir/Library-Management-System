161bcb15746d5b044adb63f72cc0fe36423794d9
Haroon Tahir <noono14252@outlook.com>
2021-10-22 23:58:46 +0500

    Created Book Table

--- Library Management System/Database/Books.sql
+++ Library Management System/Database/Books.sql
@@ -4,4 +4,9 @@ CREATE TABLE Book (
 	/*
 		Book_ID, Book_Name, Book_Type, Author Name, Category
 	*/
+	[Book ID] INT NOT NULL PRIMARY KEY,
+	[Book Name] VARCHAR(20) NOT NULL,
+	[Book Type] VARCHAR(20) CHECK([Book Type] IN ('Premium', 'Regular')),
+	[Author Name] VARCHAR(30) NOT NULL,
+	Category VARCHAR(20),
 )
